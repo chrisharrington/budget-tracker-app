17765a2a84218a325cdbd4ef59afc79028adf8a5
Chris Harrington <chrisharrington99@gmail.com>
2024-01-28 15:41:49 +0000

    Add error component.

---
+++ lib/components/error.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import Colours from '@lib/colours';
+
+type Props = {
+    error: Error | string;
+}
+
+export const Error = ({ error } : Props) => {
+    return <View style={styles.container}>
+        <Text style={styles.text}>
+            {(error as Error).stack ? (error as Error).stack : error.toString()}
+        </Text>
+    </View>;
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: Colours.background.error,
+        borderRadius: 5,
+        padding: 15,
+        paddingTop: 15 + (StatusBar.currentHeight || 0)
+    },
+
+    text: {
+        color: Colours.text.default
+    },
+});
